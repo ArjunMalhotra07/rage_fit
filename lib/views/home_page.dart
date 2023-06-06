d1e296dbf405e2f4fd7d04f5c10c9ca54b39aa94
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2023-06-06 12:08:30 +0530

    Added icons

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -8,17 +8,57 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Work Out Logs'),
-          centerTitle: true,
-          backgroundColor: Colors.blue.shade600,
+          backgroundColor: appColors.cardColor,
         ),
         body: Container(
             color: Colors.black,
-            child: ListView.builder(
-                itemCount: listOfWorkOuts.length,
-                itemBuilder: (context, index) {
-                  return Container();
-                })));
+            child: Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: ListView.builder(
+                  itemCount: listOfWorkOuts.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      color: appColors.cardColor,
+                      child: Container(
+                        padding: const EdgeInsets.only(left: 10),
+                        margin: const EdgeInsets.all(10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  appUtilities
+                                      .formatWorkOutType(listOfWorkOuts[index]),
+                                  style: const TextStyle(
+                                      color: Colors.white, fontSize: 18),
+                                ),
+                                const Spacer(),
+                                const Icon(Icons.question_mark_sharp,
+                                    color: Colors.white54),
+                                const SizedBox(width: 10),
+                                const Icon(Icons.more_vert,
+                                    color: Colors.white54)
+                              ],
+                            ),
+                            const SizedBox(height: 10),
+                            Row(
+                              children: [
+                                const Icon(Icons.add),
+                                const SizedBox(width: 10),
+                                Text(
+                                  'Warm-Up',
+                                  style: TextStyle(
+                                      color: Colors.red.shade300, fontSize: 16),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  }),
+            )));
   }
 }
 /*ListView.builder(
