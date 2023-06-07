9f2b21de798883dc0dec32a2c9eede1ec1ba8fee
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2023-06-07 20:45:20 +0530

    removed unused import

--- lib/views/templates/workout_tile.dart
+++ lib/views/templates/workout_tile.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:rage_fit/logic/workout_provider.dart';
-import 'package:rage_fit/views/templates/workout_header_tile.dart';
 import '../../models/enum.dart';
 import 'constants.dart';
 import '../../models/work_out.dart';
